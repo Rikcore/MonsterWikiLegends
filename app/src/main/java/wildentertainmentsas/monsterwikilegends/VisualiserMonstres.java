8962e02b586598adc5e04e14ce85ab78368e4f4a
[Rikcore] <[cyprien.chamayou@gmail.com]>
2017-02-28 14:15:14 +0100

    ajout element

--- app/src/main/java/wildentertainmentsas/monsterwikilegends/VisualiserMonstres.java
+++ app/src/main/java/wildentertainmentsas/monsterwikilegends/VisualiserMonstres.java
@@ -5,31 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class Creation extends AppCompatActivity {
-
-    private
+public class VisualiserMonstres extends AppCompatActivity {
 
     Button retour;
-    Button valider;
-    EditText nommer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creation);
+        setContentView(R.layout.activity_visualiser_monstres);
 
         retour = (Button) findViewById(R.id.retour);
-        valider = (Button) findViewById(R.id.valider);
-        nommer = (EditText) findViewById(R.id.edittextnommer);
-
-
 
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Creation.this, Accueil.class));
+                startActivity(new Intent(VisualiserMonstres.this, Accueil.class));
             }
         });
     }
